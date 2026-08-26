8108ecb3f30031816b7d3cc524c39da0f6cec3d6
ddrsistemas <ddrsistemas@gmail.com>
2026-08-26 17:31:59 -0400

    subir

--- database/seeders/MedicalDataSeeder.php
+++ database/seeders/MedicalDataSeeder.php
@@ -55,7 +55,7 @@ class MedicalDataSeeder extends Seeder
             ['first_name' => 'Ana', 'last_name' => 'Pérez', 'email' => 'ana.perez@example.com', 'phone' => '04121112233', 'password' => bcrypt('12345678'), 'reg_medico' => 'MPPS-54321'],
             ['first_name' => 'María', 'last_name' => 'Delgado', 'email' => 'maria.delgado@example.com', 'phone' => '04122223344', 'password' => bcrypt('12345678'), 'reg_medico' => 'MPPS-98765'],
             ['first_name' => 'Alejandro', 'last_name' => 'Rojas', 'email' => 'alejandro.rojas@example.com', 'phone' => '04243334455', 'password' => bcrypt('12345678'), 'reg_medico' => 'MPPS-56789'],
-            ['first_name' => 'Elena', 'last_name' => 'Benítez', 'email' => 'elena.benitez@example.com', 'phone' => '04164445566', 'password' => bcrypt('12345678'), 'reg_medico' => 'MPPS-13579'],
+            ['first_name' => 'Elena', 'last_name' => 'Benítez', 'email' => 'elena.benitez@example.com', 'phone' => '04164445566', 'password' => bcrypt('12345678'), 'reg_medico' => 'MPPS-13578'],
             ['first_name' => 'Roberto', 'last_name' => 'Gómez', 'email' => 'roberto.gomez@example.com', 'phone' => '04145556677', 'password' => bcrypt('12345678'), 'reg_medico' => 'MPPS-24680'],
             ['first_name' => 'Sofía', 'last_name' => 'López', 'email' => 'sofia.lopez@example.com', 'phone' => '04126667788', 'password' => bcrypt('12345678'), 'reg_medico' => 'MPPS-13579'],
         ];
