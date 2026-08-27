eaaba0e5adb2a3f7611fdea3b986e888c1335c11
ddrsistemas <ddrsistemas@gmail.com>
2026-08-26 22:06:05 -0400

    subir

--- routes/web/medico.php
+++ routes/web/medico.php
@@ -11,9 +11,9 @@ use App\Http\Livewire\Medico\ListPacientes;
 
 Route::middleware(['auth'])->group(function () {
     
-    // Rutas de Administración de Agenda Médica (Acceso exclusivo Root)
+    // Al colocar name('medico.'), la sub-ruta solo requiere name('pacientes')
     Route::middleware(['role:Medico'])->prefix('medico')->name('medico.')->group(function () {
-        Route::get('/pacientes', ListPacientes::class)->name('medico.pacientes');
+        Route::get('/pacientes', ListPacientes::class)->name('pacientes');
     });
 
 });
