dec741afbb0456a4a952efff55a3c572f4e209de
ddrsistemas <ddrsistemas@gmail.com>
2026-08-29 00:53:27 -0400

    subir

--- app/Http/Livewire/Medico/ListPacientes.php
+++ app/Http/Livewire/Medico/ListPacientes.php
@@ -256,8 +256,6 @@ class ListPacientes extends Component
 
         // 1. Obtener el Modelo del Médico autenticado buscando por la relación user_id -> Auth::id()
         $medico = Medico::where('user_id', Auth::id())->first();
-        dd($medico); // Depuración para verificar el valor de $medico
-
         if ($medico) {
             // 2. Con el ID del Medico encontrado ($medico->id), obtenemos la lista de Pacientes asociados mediante su relación BelongsToMany
             $pacientes = $medico->pacientes()
