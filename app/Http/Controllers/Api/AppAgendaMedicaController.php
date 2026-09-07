78f6f40f53d094d5f3db5785e100a7493e8e95db
ddrsistemas <ddrsistemas@gmail.com>
2026-09-06 21:28:53 -0400

    subir

--- app/Http/Controllers/Api/AppAgendaMedicaController.php
+++ app/Http/Controllers/Api/AppAgendaMedicaController.php
@@ -123,9 +123,17 @@ class AppAgendaMedicaController extends Controller
                     $finMes = Carbon::now()->endOfMonth()->format('Y-m-d');
                 }
 
-                // 6. Obtener Pacientes, Consultas y Colas según el tipo de usuario
+                // Inicializar colección de centros médicos vacía
+                $centrosMedicos = \collect([]);
+
+                // 6. Obtener Pacientes, Consultas, Colas y Centros Médicos según el tipo de usuario
                 if ($userType === 'Medico' || ($userType === 'Root' && $medicoModel)) {
                     
+                    // Cargar los centros médicos vinculados al médico
+                    if ($medicoModel) {
+                        $centrosMedicos = $medicoModel->medicalCenters()->withPivot('reg_medico')->get();
+                    }
+
                     // Recopilar reg_medico desde MedicoRegistro y el modelo Medico
                     $registrosMedicos = MedicoRegistro::where('medico_id', $medicoModel->id)
                         ->pluck('reg_medico')
@@ -188,6 +196,7 @@ class AppAgendaMedicaController extends Controller
                     $pacientesRaw = Paciente::all();
                     $consultas = Consulta::whereBetween('fecha', [$inicioMes, $finMes])->get();
                     $colas = Cola::whereBetween('fecha', [$inicioMes, $finMes])->get();
+                    $centrosMedicos = \App\Models\MedicalCenter::all();
                 }
 
                 // Mapear los pacientes para la respuesta JSON
@@ -244,6 +253,7 @@ class AppAgendaMedicaController extends Controller
                     'colas'                   => $colas,
                     'pacientes'               => $pacientes->values(),
                     'motivos'                 => $motivos,
+                    'centros_medicos'         => $centrosMedicos,
                     'capacidad_diaria_maxima' => 8
                 ], 200);
 
