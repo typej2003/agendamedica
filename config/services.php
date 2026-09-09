d5627b0831b37bbeaac60235da914969419afdca
ddrsistemas <ddrsistemas@gmail.com>
2026-09-08 20:00:09 -0400

    subir

--- config/services.php
+++ config/services.php
@@ -30,4 +30,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'whatsapp' => [
+        'token' => env('WHATSAPP_TOKEN'),
+        'phone_number_id' => env('WHATSAPP_PHONE_NUMBER_ID'),
+        'verify_token' => env('WHATSAPP_VERIFY_TOKEN'),
+    ],
+
 ];
